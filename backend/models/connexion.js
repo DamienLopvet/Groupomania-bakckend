c903ef8b09c360b0e7f8cdb335fadc0067efe74d
Twins corporation <etwincorp@gmail.com>
2022-02-03 08:26:03 +0100

    vars env setup

--- backend/models/connexion.js
+++ backend/models/connexion.js
@@ -8,10 +8,10 @@ const db = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
   pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle,
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
   },
 });
 
